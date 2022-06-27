c39ddca1683e89d23ce1216068fc32da865c36d1
MohamedAdelll <97407829+MohamedAdelll@users.noreply.github.com>
2022-06-27 13:24:13 +0200

    Delete static/scripts/export default async function getPatients() {   const patientDataP = await fetch("/static directory

--- static/scripts/export default async function getPatients() {   const patientDataP = await fetch("/static/getPatient.js
+++
@@ -1,5 +0,0 @@
-export default async function getPatients() {
-  const patientDataP = await fetch("/static/json_patients.json");
-  const patientDataJson = await patientDataP.json();
-  return patientDataJson;
-}
